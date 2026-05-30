337a547623ed5a6c0e234f7e5441299e17c0a97d
ArmanKadian-sys <armankadian11@gmail.com>
2026-05-30 17:30:05 +1000

    Forgot to add express.json

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 const BANNER_KEY="arman_redis_banner";
 
 
-
+app.use(express.json());
 app.post("/banner", async(req, res)=>{
   await redis.set(BANNER_KEY, req.body.message||"Random Banner Value");
   res.json({status: true});
